23b58a78cf3eba6bf07e9183c06fa6ccc1f3cec9
Charlie NoFace <youremail@gmail.comh.e.ergaster@gmail.comgit config --global user.email youremail@gmail.com>
2021-07-07 00:52:31 -0700

    added basic client side routing

--- src/app.js
+++ src/app.js
@@ -6,9 +6,9 @@ import "./styles/styles.scss";
 
 const ExpenseDashboardPage = () => (
   <div>
-  This is from my dashboard component. Hello. Goodbye
+    This is from my dashboard component.
   </div>
-)
+);
 
 const AddExpensePage = () => (
   <div>This is from my add expense component</div>
@@ -17,23 +17,36 @@ const EditExpensePage = () => (
   <div>This is from my edit expense components</div>
 );
 const HelpMe = () => (
-  <div>Please help me!</div>
+  <div>Please help me!
+  </div>
 );
 const NotFoundPage = () => (
   <div>THIS AN 404! - <Link to="/">Get Me Out of Here</Link>
   </div>
 );
 
+const Header = () => (
+  <header>
+    <h1>Coin Keeper</h1>
+    <Link to="/">Home</Link>
+    <Link to="/create">Add Expense</Link>
+    <Link to="/edit">Edit Expense</Link>
+    <Link to="/help">Get Help</Link>
+  </header>
+);
 
 const routes = (
   <BrowserRouter>
-    <Switch>
-      <Route path="/" component={ExpenseDashboardPage} exact={true} />
-      <Route path="/create" component={AddExpensePage} />
-      <Route path="/edit" component={EditExpensePage} />
-      <Route path="/help" component={HelpMe} />
-      <Route component={NotFoundPage} />
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/" component={ExpenseDashboardPage} exact={true} />
+        <Route path="/create" component={AddExpensePage} />
+        <Route path="/edit" component={EditExpensePage} />
+        <Route path="/help" component={HelpMe} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
   </BrowserRouter>
 );
 
