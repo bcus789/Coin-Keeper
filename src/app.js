a5b60c09953f3873cb084134c7cdc26e18218981
Charlie NoFace <youremail@gmail.comh.e.ergaster@gmail.comgit config --global user.email youremail@gmail.com>
2021-07-07 00:24:16 -0700

    added switch to browser router

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
@@ -26,13 +26,13 @@ const NotFoundPage = () => (
 
 const routes = (
   <BrowserRouter>
-    <div>
+    <Switch>
       <Route path="/" component={ExpenseDashboardPage} exact={true} />
       <Route path="/create" component={AddExpensePage} />
       <Route path="/edit" component={EditExpensePage} />
       <Route path="/help" component={HelpMe} />
       <Route component={NotFoundPage} />
-    </div>
+    </Switch>
   </BrowserRouter>
 );
 
